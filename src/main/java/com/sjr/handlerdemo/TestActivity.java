705bbfef4e0831f65c7af4fe17767f40e103cf21
songjiran <474918078@qq.com>
2016-05-25 15:41:07 +0800

    新增

---
+++ src/main/java/com/sjr/handlerdemo/TestActivity.java
@@ -0,0 +1,15 @@
+package com.sjr.handlerdemo;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by 宋家任 on 2016/5/25 15:39.
+ */
+public class TestActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
